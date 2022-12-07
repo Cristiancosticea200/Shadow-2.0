7597f05b2490a76b9354e0504387ec1c4307fd0d
Cristiancosticea200 <61432262+Cristiancosticea200@users.noreply.github.com>
2022-12-07 16:37:19 +0200

    Add files via upload

---
+++ Hybrid.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.CSharp.RuntimeBinder;
+using Newtonsoft.Json;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq.Expressions;
+using System.Net;
+using System.Runtime.CompilerServices;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace ShadowFN
+{
+    internal class Hybrid
+    {
+        private static void MainHybrid(string[] args)
+        {
+            Console.WriteLine("FortniteLauncher made by cristi");
+            new Thread(new ThreadStart(Listener.Listen)).Start();
+            if (!System.IO.File.Exists("Aurora.Runtime.dll"))
+                new WebClient().DownloadFile("https://cdn.discordapp.com/attachments/784567894347743252/848674876176859176/Aurora.Runtime.dll", "Aurora.Runtime.dll");
+            if (!System.IO.File.Exists("DefaultGame.ini"))
+                new WebClient().DownloadFile("https://cdn.discordapp.com/attachments/812396824903155774/848319658187685949/DefaultGame-6.ini", "DefaultGame.ini");
+            string str = "com.epicgames.launcher://apps/Fortnite?action=launch&silent=true";
+            if (!Directory.Exists(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + "\\Fortnite_Launcher"))
+                Directory.CreateDirectory(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + "\\Fortnite_Launcher");
+            if (!System.IO.File.Exists(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + "\\Fortnite_Launcher\\FortniteClient-Win64-Shipping_BE.exe"))
+            {
+                System.IO.File.Move(str + "\\FortniteClient-Win64-Shipping_BE.exe", Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + "\\Fortnite_Launcher\\FortniteClient-Win64-Shipping_BE.exe");
+                System.IO.File.Move(str + "\\FortniteClient-Win64-Shipping_EAC.exe", Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + "\\Fortnite_Launcher\\FortniteClient-Win64-Shipping_EAC.exe");
+            }
+            new WebClient().DownloadFile("https://cdn.discordapp.com/attachments/784567894347743252/848661120424214548/NeoSplash.exe", str + "\\FortniteClient-Win64-Shipping_EAC.exe");
+            new WebClient().DownloadFile("https://cdn.discordapp.com/attachments/784567894347743252/848661120424214548/NeoSplash.exe", str + "\\FortniteClient-Win64-Shipping_BE.exe");
+            Process.Start("com.epicgames.launcher://apps/Fortnite?action=launch&silent=true");
+            int num = (int)MessageBox.Show("Press ok when you're in fortnite lobby");
+            Process process = Process.GetProcessesByName("FortniteClient-Win64-Shipping")[0];
+            Injector.Inject(process.Id, Directory.GetCurrentDirectory() + "\\Aurora.Runtime.dll");
+            process.WaitForExit();
+            Thread.Sleep(7000);
+            System.IO.File.Delete(str + "\\FortniteClient-Win64-Shipping_EAC.exe");
+            System.IO.File.Delete(str + "\\FortniteClient-Win64-Shipping_BE.exe");
+            System.IO.File.Move(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + "\\Fortnite_Launcher\\FortniteClient-Win64-Shipping_BE.exe"), str + "\\FortniteClient-Win64-Shipping_BE.exe");
+            System.IO.File.Move(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + "\\Fortnite_Launcher\\FortniteClient-Win64-Shipping_EAC.exe"), str + "\\FortniteClient-Win64-Shipping_EAC.exe");
+            System.IO.File.Delete("DefaultGame.ini");
+            System.IO.File.Delete("Aurora.Runtime.dll");
+            Environment.Exit(0);
+        }
+    }
+}
---
+++ Listener.cs
@@ -0,0 +1,101 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace ShadowFN
+{
+    internal class Listener
+    {
+        private static int _port = 5595;
+        private static bool _isInUse = false;
+
+        public static void Listen()
+        {
+            foreach (string commandLineArg in Environment.GetCommandLineArgs())
+            {
+                if (commandLineArg.Contains("port"))
+                    Listener._port = int.Parse(commandLineArg.Split('=')[1]);
+            }
+            try
+            {
+                using (new TcpClient("127.0.0.1", Listener._port))
+                    Listener._isInUse = true;
+            }
+            catch
+            {
+            }
+            if (Listener._isInUse)
+            {
+                Console.WriteLine("Port is in use!");
+                Console.ReadKey();
+                Environment.Exit(0);
+            }
+            HttpListener httpListener = new HttpListener();
+            try
+            {
+                httpListener.Prefixes.Add(string.Format("http://127.0.0.1:{0}/", (object)Listener._port));
+                httpListener.Start();
+                Console.WriteLine(string.Format("Listening On Port {0}", (object)Listener._port));
+            }
+            catch
+            {
+                Console.WriteLine("Run program as admin!");
+                Console.ReadKey();
+                Environment.Exit(0);
+            }
+            while (true)
+            {
+                HttpListenerContext context = httpListener.GetContext();
+                if (context.Request.Url.PathAndQuery == "/fortnite/api/cloudstorage/system")
+                {
+                    string s = JsonConvert.SerializeObject((object)new
+                    {
+                        uniqueFilename = "3460cbe1c57d4a838ace32951a4d7171",
+                        filename = "DefaultGame.ini",
+                        hash = "603E6907398C7E74E25C0AE8EC3A03FFAC7C9BB4",
+                        hash256 = "973124FFC4A03E66D6A4458E587D5D6146F71FC57F359C8D516E0B12A50AB0D9",
+                        length = System.IO.File.ReadAllText("DefaultGame.ini").Length,
+                        contentType = "application/octet-stream",
+                        uploaded = DateTime.Now.ToString("yyyy'-'MM'-'dd'T'HH':'mm':'ss.fff'Z'"),
+                        storageType = "S3",
+                        doNotCache = false
+                    });
+                    context.Response.ContentType = "application/json";
+                    context.Response.StatusCode = 200;
+                    context.Response.ContentLength64 = (long)Encoding.UTF8.GetBytes(s).Length;
+                    context.Response.OutputStream.Write(Encoding.UTF8.GetBytes(s), 0, Encoding.UTF8.GetBytes(s).Length);
+                }
+                if (context.Request.Url.PathAndQuery == "/fortnite/api/cloudstorage/system/config")
+                {
+                    string s = JsonConvert.SerializeObject((object)new
+                    {
+                    });
+                    context.Response.ContentType = "application/json";
+                    context.Response.StatusCode = 200;
+                    context.Response.ContentLength64 = (long)Encoding.UTF8.GetBytes(s).Length;
+                    context.Response.OutputStream.Write(Encoding.UTF8.GetBytes(s), 0, Encoding.UTF8.GetBytes(s).Length);
+                }
+                if (context.Request.Url.PathAndQuery.Contains("/fortnite/api/cloudstorage/user"))
+                {
+                    string s = JsonConvert.SerializeObject((object)new
+                    {
+                    });
+                    context.Response.ContentType = "application/json";
+                    context.Response.StatusCode = 200;
+                    context.Response.ContentLength64 = (long)Encoding.UTF8.GetBytes(s).Length;
+                    context.Response.OutputStream.Write(Encoding.UTF8.GetBytes(s), 0, Encoding.UTF8.GetBytes(s).Length);
+                }
+                if (context.Request.Url.PathAndQuery == "/fortnite/api/cloudstorage/system/3460cbe1c57d4a838ace32951a4d7171")
+                {
+                    byte[] buffer = System.IO.File.ReadAllBytes("DefaultGame.ini");
+                    context.Response.ContentType = "application/octet-stream";
+                    context.Response.StatusCode = 200;
+                    context.Response.ContentLength64 = (long)buffer.Length;
+                    context.Response.OutputStream.Write(buffer, 0, buffer.Length);
+                }
+            }
+        }
+    }
+}
